e9b90eff1968578588d089ce7371d54767384cc5
akhilmike <89603470+akhilmike@users.noreply.github.com>
2021-11-11 18:33:54 -0600

    n

--- routes/icecream.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('icecream', { title: 'Search Results icecream ' });
-});
-
-module.exports = router;
