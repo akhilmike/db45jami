01e550f8afb01fbfb3a6aca2e04de96da1efa079
akhilmike <89603470+akhilmike@users.noreply.github.com>
2021-11-11 22:59:08 -0600

    Update icecream.js

--- controllers/icecream.js
+++ controllers/icecream.js
@@ -13,8 +13,15 @@ exports.icecream_list = async function(req, res) {
 }; 
  
 // for a specific icecream. 
-exports.icecream_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: icecream detail: ' + req.params.id); 
+exports.icecream_detail =  async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await icecream.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
 // Handle icecream create on POST. 
@@ -44,8 +51,22 @@ exports.icecream_delete = function(req, res) {
 }; 
  
 // Handle icecream update form on PUT. 
-exports.icecream_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: icecream update PUT' + req.params.id); 
+exports.icecream_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await icecream.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.flavour)  
+               toUpdate.flavour = req.body.flavour; 
+        if(req.body.price) toUpdate.price = req.body.price; 
+        if(req.body.toppings) toUpdate.toppings = req.body.toppings; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
+    } 
 }; 
 
 // VIEWS 
