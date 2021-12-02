885fdb7b4c906ec6f4da067523f3a4dd523a1a68
akhilmike <89603470+akhilmike@users.noreply.github.com>
2021-12-01 23:53:52 -0600

    committed files

--- models/icecream.js
+++ models/icecream.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose") 
-const icecreamSchema = mongoose.Schema({ flavour: String ,price: Number,toppings:String}) 
+const icecreamSchema = mongoose.Schema({ flavour: {type:String,minlength:5} ,price: {type:Number,min:6,max:11} ,toppings:String}) 
  
 module.exports = mongoose.model("icecream", icecreamSchema) 
--- routes/icecream.js
+++ routes/icecream.js
@@ -2,6 +2,14 @@ var express = require('express');
 const icecream_controlers= require('../controllers/icecream'); 
 var router = express.Router();
 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 /* GET home page. */
 router.get('/', icecream_controlers.icecream_view_all_Page );
 
@@ -9,11 +17,11 @@ router.get('/', icecream_controlers.icecream_view_all_Page );
 router.get('/detail', icecream_controlers.icecream_view_one_Page); 
  
 /* GET create icecream page */ 
-router.get('/create', icecream_controlers.icecream_create_Page); 
+router.get('/create',secured, icecream_controlers.icecream_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', icecream_controlers.icecream_update_Page); 
+router.get('/update',secured, icecream_controlers.icecream_update_Page); 
 
 /* GET create icecream page */
-router.get('/delete', icecream_controlers.icecream_delete_Page);
+router.get('/delete',secured, icecream_controlers.icecream_delete_Page);
 module.exports = router;
